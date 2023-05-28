1cf7bdf1c1116887938e6c5e3131d4363a02cd4c
EnriqueVerdi <verdicruzenrique@gmail.com>
2023-05-28 14:36:34 -0600

    Fix(task): Add of database

--- src/task/infrastructure/database/db-connection.ts
+++ src/task/infrastructure/database/db-connection.ts
@@ -1,11 +1,11 @@
 import { Pool } from "pg";
 
 const config = {
-  host: "localhost",
+  host: "database-1.c9meh61fegg7.us-east-1.rds.amazonaws.com",
   port: 5432,
   user: "postgres",
-  database: "hexagonal_architecture",
-  password: "solis2002",
+  database: "postgres",
+  password: "N0Tweak$_123!",
 };
 
 export const pool = new Pool(config);
